b37f23f6069e79753dd0e2abddf3c8ef6f1d24c4
Juan Montes <juancmontes13@gmail.com>
2023-01-24 08:34:46 -0500

    complemento primera entrega

---
+++ src/components/itemListContainer/itemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemListContainer = ({Texto}) => {
+    return (
+        <>
+            <p>{Texto}</p>
+        </>
+    );
+}
+
+export default ItemListContainer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div>
              <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-             
+             <a href="#">Plant Base</a>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
               </button>
